b51883d1ea40a382527f022a5f25df75eb2b0157
Harmony <qblyf@126.com>
2026-01-14 10:41:51 +0800

    refactor: 移除编辑SPU中的ID显示区域

--- components/SPUEdit.tsx
+++ components/SPUEdit.tsx
@@ -106,7 +106,7 @@ export default function SPUEdit() {
 
   return (
     <>
-      <h2>编辑 {preData.name}</h2>
+      <h2>编辑 SPU</h2>
 
       <Form layout="vertical" autoComplete="off">
         <Tabs
