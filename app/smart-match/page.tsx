92287f6099b0fcf95d7d5c4b9080a2ce6a4b1208
Harmony <qblyf@126.com>
2026-01-21 15:22:38 +0800

    fix: 修复PageWrap组件导入错误
    - 将命名导入改为默认导入
    - 添加必需的ppKey属性

--- app/smart-match/page.tsx
+++ app/smart-match/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { PageWrap } from '../../components/PageWrap';
+import PageWrap from '../../components/PageWrap';
 import { SmartMatchComponent } from '../../components/SmartMatch';
 
 export default function SmartMatchPage() {
   return (
-    <PageWrap title="智能匹配">
+    <PageWrap ppKey="product-manage">
       <SmartMatchComponent />
     </PageWrap>
   );
